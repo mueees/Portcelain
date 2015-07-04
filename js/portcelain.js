9243196f98532dced664d8c19eb0e176410dc651
coderhtml <legenkaya.lyudmila@gmail.com>
2015-07-04 16:21:30 +0300

    corrections according to "review-comments"

--- js/portcelain.js
+++ js/portcelain.js
@@ -22,33 +22,24 @@ $(document).ready(function(){
         speed: 1300,
         autoplay: true,
         autoplaySpeed: 2000,
+        slidesToScroll: 4,
+        slidesToShow: 4,
         responsive: [
-            { breakpoint: 100000,
-                settings: {
-                    rows: 3,
-                    slidesPerRow: 1,
-                    slidesToScroll: 4,
-                    slidesToShow: 4
-                }
-            },
             { breakpoint: 800,
                 settings: {
-                    rows: 2,
-                    slidesPerRow: 1,
                     slidesToScroll: 3,
                     slidesToShow: 3
                 }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    rows: 1,
-                    slidesPerRow: 1,
-                    slidesToScroll: 2,
-                    slidesToShow: 2
-                }
             }]
     });
-
+    /** b_nav-clients-mobile **/
+    $('.b_nav-clients-mobile').slick({
+        infinite: true,
+        speed: 1300,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToScroll: 2,
+        slidesToShow: 2
+    });
 });
 
