597628a3ee084565a8dc35f5caeef53f03745be9
coderhtml <legenkaya.lyudmila@gmail.com>
2015-07-18 23:21:49 +0300

    corrections according to "review-comments"

--- js/portcelain.js
+++ js/portcelain.js
@@ -76,88 +76,41 @@ $(document).ready(function(){
 }
 
 
+
     /**navigation menu**/
+    // Show navigation menu (for devices)
+    $(document).on('click', ".open-icon-hamburger", showMenu);
 
-        // Show navigation menu (for devices)
-        $(document).on('click', ".open-icon-hamburger", showMenu);
-
-        // Hide navigation menu (for devices)
-        $(document).on('click', ".close-icon-hamburger", closeMenu);
-
-
-        function showMenu() {
-            $('.open-icon-hamburger').css('display', 'none');
-            $('.close-icon-hamburger').css('display', 'block');
-            $('.overlay').css('display', 'block');
-            $('.header-navigation').addClass('show-navigation');
-            /**$('#container').addClass('stop-scrolling');**/
-        }
-
-        function closeMenu() {
-            $('.close-icon-hamburger').css('display', 'none');
-            $('.open-icon-hamburger').css('display', 'block');
-            $('.overlay').css('display', 'none');
-            $('.header-navigation').removeClass('show-navigation');
-            /**$('#container').removeClass('stop-scrolling');**/
-        }
+    // Hide navigation menu (for devices)
+    $(document).on('click', ".close-icon-hamburger", closeMenu);
 
 
-   /** function handleNavigationMenuTabs() {
-        $(document).on('click', ".device-menu-tab", showTabContent);
+    function showMenu() {
+        $('.header-navigation').addClass('show-navigation');
+        $('.disable-link').on('click', function(e){
+            e.preventDefault();
+        });
+        $('.overlay').css('display', 'block');
 
-        function showTabContent() {
-            $('.device-menu-tab').removeClass('.active');
-            $(this).addClass('active');
-            $('.sub-menu-l2').removeClass('active');
-            if ((this).classList.contains('.mobile-item')) {
-                $('.sub-menu-2').addClass('active')
-            } else if ((this).classList.contains('links')) {
-                $('.sub-menu-2').addClass('active')
-            } else {
-                $('#navBarDocumentContent').addClass('active')
-            }
-        }
+    }
+
+    function closeMenu() {
+        $('.overlay').css('display', 'none');
+        $('.header-navigation').removeClass('show-navigation');
     }
 
 
- /**   // Show page name on Navigation Menu
-    var pageTitle = $('h1').text();
-    $('.document-page-title').append(pageTitle);
+    $(document).on('click', ".device-menu-tab", showTabContent);
 
-
-    // Generate list of the API documents for devices navigation menu
-    $.ajax({
-        type: 'get',
-        url: '/api/docs',
-        success: function (page) {
-            var re = /(<a.*wikilink1.*a>)/gi; //get all wikilinks on '/api/docs' page
-            var links = page.match(re); // create array with recived links
-            var html = '';
-            $.each(links, function (i, link) {
-                html += '<li>' + link + '</li>'; // create the string with listed links
-            });
-            $('.api-documentation-links').append(html); // append the string in DOM
-        }
-    });
-
-
-    // Calculate height of navigation menu content area height
-    function calcDeviceNavigationMenuContentHeight() {
-        var viewportHeight = $(window).height();
-        var navMenuHeaderHeight = $('.device-menu-first-line').height() + $('.device-menu-tabs-inner').height() + 10;
-        var navMenuContentHeight = viewportHeight - navMenuHeaderHeight;
-        $('.device-menu-tab-content').css('height', navMenuContentHeight);
+    function showTabContent() {
+        $('.device-menu-tab').removeClass('active');
+        $(this).addClass('active');
+        $('.sub-menu-l2').removeClass('active');
     }
-    $(window).resize(calcDeviceNavigationMenuContentHeight);
 
 
 
-    handleDeviceNavigationMenu();
-    handleNavigationMenuTabs();
-    animateScrollToTop();
-    calcDeviceNavigationMenuContentHeight()
 
-**/
 
 });
 
